ae632c90388617cc035b529b7d92e39c1cc68866
Kidus Worku <kidusmik@gmail.com>
2022-01-25 07:09:00 +0000

    implement unique file storage instance creation

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,10 @@
+"""
+This is "__init.py" module of "models" package.
+The __init__.py module makes Python treat directories\
+containing the file as packages.
+"""
 import models.engine.file_storage
+
 
 storage = FileStorage()
 storage.reload()
