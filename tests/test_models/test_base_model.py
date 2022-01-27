d3d4a0d28877ac0aaacf9f6dee89603179e8e924
Kidus Worku <kidusmik@gmail.com>
2022-01-27 07:41:18 +0000

    add unittest for base_model module

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,12 +2,9 @@
 """
 The ``test_base_model`` module
 ==============================
-
 Using ``test_base_model``
 -------------------------
-
 This is a test_base_model unittest file to test the base_model module.
-
 """
 import unittest
 from models.base_model import BaseModel
@@ -18,9 +15,10 @@ import os
 
 class TestBaseModel(unittest.TestCase):
     """Defines a class TestBaseModel.
-
     Test functionality of the base_model module.
     """
+    a = [BaseModel, "BaseModel"]
+
     def tearDown(self):
         try:
             os.remove('file.json')
@@ -29,40 +27,41 @@ class TestBaseModel(unittest.TestCase):
 
     def test_create(self):
         """Test if the created class is same type."""
-        a = BaseModel()
-        self.assertIsInstance(a, BaseModel)
+        obj = TestBaseModel.a[0]()
+        self.assertIsInstance(obj, TestBaseModel.a[0])
 
     def test_id(self):
         """Tests if id is of type str."""
-        a = BaseModel()
-        self.assertEqual(type(a.id), str)
+        obj = TestBaseModel.a[0]()
+        self.assertEqual(type(obj.id), str)
 
     def test_time(self):
         """Tests if the created and updated date is equal at creation."""
-        a = BaseModel()
-        self.assertEquals(a.created_at, a.updated_at)
+        obj = TestBaseModel.a[0]()
+        self.assertEqual(obj.created_at, obj.updated_at)
 
     def test_created_at(self):
         """Tests if created_at is of type datetime."""
-        a = BaseModel()
-        self.assertEqual(type(a.created_at), datetime.datetime)
+        obj = TestBaseModel.a[0]()
+        self.assertEqual(type(obj.created_at), datetime.datetime)
 
     def test_updated_at(self):
         """Test is updated_at is of type datetime."""
-        a = BaseModel()
-        self.assertEqual(type(a.updated_at), datetime.datetime)
+        obj = TestBaseModel.a[0]()
+        self.assertEqual(type(obj.updated_at), datetime.datetime)
 
     def test_str(self):
         """Tests if the string representation is correct format."""
-        obj = BaseModel()
-        self.assertEqual(str(obj), '[{}] ({}) {}'.format("BaseModel", obj.id,
+        obj = TestBaseModel.a[0]()
+        self.assertEqual(str(obj), '[{}] ({}) {}'.format(TestBaseModel.a[1],
+                         obj.id,
                          obj.__dict__))
 
     def test_save(self):
         """Tests if the saved attributes are the same as the original."""
-        obj = BaseModel()
+        obj = TestBaseModel.a[0]()
         obj.save()
-        key = "BaseModel" + "." + obj.id
+        key = TestBaseModel.a[1] + "." + obj.id
         with open('file.json', 'r') as f:
             j = json.load(f)
             self.assertEqual(j[key], obj.to_dict())
